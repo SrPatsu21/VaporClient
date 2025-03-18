a112a2342e2539ff654414986b74bdf3cb6c2d72
matheuswogt37 <matheusnotas07@gmail.com>
2025-03-18 11:19:44 -0300

    electron initial setup add

--- app/index.js
+++ app/index.js
@@ -1,1 +1,32 @@
-console.log("Hello world!");
+const { app, screen, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+    const primaryDisplay = screen.getPrimaryDisplay();
+    console.log(primaryDisplay.workAreaSize);
+    const win = new BrowserWindow({
+        width: primaryDisplay.workAreaSize.width,
+        height: primaryDisplay.workAreaSize.height,
+    });
+
+    win.loadFile("index.html");
+};
+
+//* if all windows are closed then close the process app
+//? windows and linux - FUNCIONALITY
+if (process.platform !== "darwin") {
+    app.on("window-all-closed", () => {
+        app.quit();
+    });
+}
+
+//* when start app then create an windows
+app.whenReady().then(() => {
+    createWindow();
+
+    //? macos - FUNCIONALITY
+    if (process.platform == "darwin") {
+        app.on("activate", () => {
+            if (BrowserWindow.getAllWindows().length === 0) createWindow();
+        });
+    }
+});
