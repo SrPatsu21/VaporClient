250cdb6ddb72756d2eadff5d739554c55b01e5eb
matheuswogt37 <matheusnotas07@gmail.com>
2025-03-19 10:11:13 -0300

    update function to stop process if don't have any windows open

--- app/index.js
+++ app/index.js
@@ -1,32 +1,28 @@
 const { app, screen, BrowserWindow } = require("electron");
 
+/*
+? app - create and manage electron app
+? screen - get screen size to init with monitor size
+? BrowserWindow - create and manage window
+*/
+
 const createWindow = () => {
-    const primaryDisplay = screen.getPrimaryDisplay();
-    console.log(primaryDisplay.workAreaSize);
+    //? get primary monitor size
+    const primaryDisplay = screen.getPrimaryDisplay().workAreaSize;
     const win = new BrowserWindow({
-        width: primaryDisplay.workAreaSize.width,
-        height: primaryDisplay.workAreaSize.height,
+        width: primaryDisplay.width,
+        height: primaryDisplay.height,
     });
 
     win.loadFile("index.html");
 };
 
 //* if all windows are closed then close the process app
-//? windows and linux - FUNCIONALITY
-if (process.platform !== "darwin") {
-    app.on("window-all-closed", () => {
-        app.quit();
-    });
-}
+app.on("window-all-closed", () => {
+    app.quit();
+});
 
 //* when start app then create an windows
 app.whenReady().then(() => {
     createWindow();
-
-    //? macos - FUNCIONALITY
-    if (process.platform == "darwin") {
-        app.on("activate", () => {
-            if (BrowserWindow.getAllWindows().length === 0) createWindow();
-        });
-    }
 });
