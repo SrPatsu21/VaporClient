e2ada7441447045533ea9153d409b94c0a565db3
GordaoBF <bonniethenightmaretm64@gmail.com>
2025-03-27 21:32:34 -0300

    Added Tailwindcss

--- app/index.js
+++ app/index.js
@@ -5,18 +5,7 @@ const { app, screen, BrowserWindow } = require("electron");
 ? screen - get screen size to init with monitor size
 ? BrowserWindow - create and manage window
 */
-const check=false
-
-check()
-    if(check==false){
-        check==true;
-    }else{
-        check=true;
-    }
-    
-
-
-const createWindow = () => {
+const createWindow = () => {''
     //? get primary monitor size
     const primaryDisplay = screen.getPrimaryDisplay().workAreaSize;
     const win = new BrowserWindow({
@@ -25,16 +14,6 @@ const createWindow = () => {
     });
     win.loadFile("index.html");
 };
-
-//create a small window 
-const createMicroWindow = () => {
-    const win = new BrowserWindow({
-        width: 600,
-        height: 600,
-    });
-    win.loadFile("indexMicro.html");
-};
-
 //* if all windows are closed then stop the process app
 app.on("window-all-closed", () => {
     app.quit();
@@ -43,7 +22,4 @@ app.on("window-all-closed", () => {
 //* when start app then create an windows
 app.whenReady().then(() => {
     createWindow();
-    ifc(check==true){
-        createMicroWindow
-    }
 });
